591f389ebfdcf130c819107b3e52c5d826ec5d5f
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2013-11-07 16:52:11 +0000

    Working in Virgo

--- org.geppetto.simulator.moonorbit/src/main/java/org/geppetto/simulator/moonorbit/MoonOrbitModelInterpreter.java
+++ org.geppetto.simulator.moonorbit/src/main/java/org/geppetto/simulator/moonorbit/MoonOrbitModelInterpreter.java
@@ -1,7 +1,10 @@
 package org.geppetto.simulator.moonorbit;
 
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.geppetto.core.model.IModel;
 import org.geppetto.core.model.IModelInterpreter;
@@ -11,21 +14,45 @@ import org.geppetto.core.visualisation.model.Entity;
 import org.geppetto.core.visualisation.model.Point;
 import org.geppetto.core.visualisation.model.Scene;
 import org.geppetto.core.visualisation.model.Sphere;
-import org.geppetto.simulator.moonorbit.JsonReader;
 import org.geppetto.simulator.moonorbit.model.MoonOrbitModel;
 import org.geppetto.simulator.moonorbit.model.Satellite;
 import org.geppetto.simulator.moonorbit.model.Vector3D;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import com.google.gson.Gson;
 
 /**
  * @author matteocantarelli
- *
+ * 
  */
+@Service
 public class MoonOrbitModelInterpreter implements IModelInterpreter
 {
 
-	/* (non-Javadoc)
+	private static String readUrl(String urlString) throws Exception
+	{
+		BufferedReader reader = null;
+		try
+		{
+			URL url = new URL(urlString);
+			reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			StringBuffer buffer = new StringBuffer();
+			int read;
+			char[] chars = new char[1024];
+			while((read = reader.read(chars)) != -1)
+				buffer.append(chars, 0, read);
+
+			return buffer.toString();
+		}
+		finally
+		{
+			if(reader != null) reader.close();
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.geppetto.core.model.IModelInterpreter#readModel(java.net.URL)
 	 */
 	@Override
@@ -33,21 +60,16 @@ public class MoonOrbitModelInterpreter implements IModelInterpreter
 	{
 		try
 		{
-			JSONObject json = JsonReader.readJsonFromUrl(url.toString());
-			
-			MoonOrbitModel mom=new MoonOrbitModel(
-					readSatellite(json,"moon"),
-					readSatellite(json,"earth"),
-					readSatellite(json,"luna3")
-			
-					);
+			Gson gson = new Gson();
+			String jsonString = readUrl(url.toString());
+			Map<String,String> json=new HashMap<String,String>();
+			json=(Map<String,String>) gson.fromJson(jsonString, json.getClass());
+			MoonOrbitModel mom = new MoonOrbitModel(readSatellite(json, "moon"), readSatellite(json, "earth"), readSatellite(json, "luna3")
+
+			);
 			return mom;
 		}
-		catch(JSONException e)
-		{
-			throw new ModelInterpreterException(e);
-		}
-		catch(IOException e)
+		catch(Exception e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -55,35 +77,40 @@ public class MoonOrbitModelInterpreter implements IModelInterpreter
 		return null;
 	}
 
-	private Satellite readSatellite(JSONObject obj,String satellite)
+	private Satellite readSatellite(Map obj, String satellite)
 	{
-		Satellite s=new Satellite();
-		JSONObject js=obj.getJSONObject(satellite);
-		Vector3D position=readVector3D(js.getJSONObject("position"));
-		Vector3D velocity=readVector3D(js.getJSONObject("velocity"));
+		Satellite s = new Satellite();
+		Map js = (Map) obj.get(satellite);
+		Vector3D position = readVector3D((Map) js.get("position"));
+		Vector3D velocity = readVector3D((Map) js.get("velocity"));
 		s.setPosition(position);
 		s.setVelocity(velocity);
-		s.setMass(js.getDouble("mass"));
+		s.setMass((double)js.get("mass"));
 		return s;
 	}
 
-	private Vector3D readVector3D(JSONObject obj)
+	private Vector3D readVector3D(Map obj)
 	{
-		return new Vector3D(obj.getDouble("x"), obj.getDouble("y"), obj.getDouble("z"));
+		return new Vector3D((double)obj.get("x"),(double)obj.get("y"),(double)obj.get("z"));
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.geppetto.core.model.IModelInterpreter#getSceneFromModel(org.geppetto.core.model.IModel, org.geppetto.core.model.state.StateTreeRoot)
 	 */
 	@Override
 	public Scene getSceneFromModel(IModel model, StateTreeRoot stateTree) throws ModelInterpreterException
 	{
-		MoonOrbitModel mo=(MoonOrbitModel) model;
-		Scene s=new Scene();
-		Entity space=new Entity();
+		MoonOrbitModel mo = (MoonOrbitModel) model;
+		Scene s = new Scene();
+		Entity space = new Entity();
 		Sphere moon = getSphereFromSatellite(mo.getMoon());
+		moon.setRadius(5d);
 		Sphere earth = getSphereFromSatellite(mo.getEarth());
+		earth.setRadius(10d);
 		Sphere luna3 = getSphereFromSatellite(mo.getLuna3());
+		luna3.setRadius(2d);
 		space.getGeometries().add(moon);
 		space.getGeometries().add(earth);
 		space.getGeometries().add(luna3);
@@ -97,8 +124,8 @@ public class MoonOrbitModelInterpreter implements IModelInterpreter
 	 */
 	private Sphere getSphereFromSatellite(Satellite moon)
 	{
-		Sphere s=new Sphere();
-		Point p=new Point();
+		Sphere s = new Sphere();
+		Point p = new Point();
 		p.setX(moon.getPosition().getX());
 		p.setY(moon.getPosition().getY());
 		p.setZ(moon.getPosition().getZ());
