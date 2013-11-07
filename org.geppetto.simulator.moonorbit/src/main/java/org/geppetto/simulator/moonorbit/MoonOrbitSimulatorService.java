431992584fd2461217d2a7d6edbca77cb0e7b8dd
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2013-11-07 15:06:57 +0000

    First commit

--- org.geppetto.simulator.moonorbit/src/main/java/org/geppetto/simulator/moonorbit/MoonOrbitSimulatorService.java
+++ org.geppetto.simulator.moonorbit/src/main/java/org/geppetto/simulator/moonorbit/MoonOrbitSimulatorService.java
@@ -1,15 +1,13 @@
-package org.geppetto.simulator.moonorbit;
+package main.java.org.geppetto.simulator.moonorbit;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.geppetto.core.common.GeppettoExecutionException;
 import org.geppetto.core.common.GeppettoInitializationException;
+import org.geppetto.core.data.model.VariableList;
 import org.geppetto.core.model.IModel;
-import org.geppetto.core.model.state.StateTreeRoot;
 import org.geppetto.core.simulation.IRunConfiguration;
 import org.geppetto.core.simulation.ISimulatorCallbackListener;
 import org.geppetto.core.simulator.ASimulator;
-import org.geppetto.core.solver.ISolver;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,25 +15,31 @@ public class MoonOrbitSimulatorService extends ASimulator {
 
 	
 	private static Log logger = LogFactory.getLog(MoonOrbitSimulatorService.class);
-	
-	@Autowired
-	private ISolver sphSolver;
 
 
 	@Override
 	public void simulate(IRunConfiguration runConfiguration) throws GeppettoExecutionException
 	{
-		logger.info("SPH Simulate method invoked");
-		StateTreeRoot results=sphSolver.solve(runConfiguration);
-		getListener().stateTreeUpdated(results);
 	}
 
 	@Override
 	public void initialize(IModel model, ISimulatorCallbackListener listener) throws GeppettoInitializationException, GeppettoExecutionException
 	{
 		super.initialize(model, listener);
-		StateTreeRoot stateTree = sphSolver.initialize(model);	
-		getListener().stateTreeUpdated(stateTree);
+	}
+
+	@Override
+	public VariableList getForceableVariables()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public VariableList getWatchableVariables()
+	{
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
